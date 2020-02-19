78cd01db494dbffa4fb01a4b82510c32b39f3f49
laihaibo <2466006555@qq.com>
2020-02-19 12:07:47 +0800

    update to new version

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
-var postcss = require('gulp-postcss');
-var gulp = require('gulp');
-var autoprefixer = require('autoprefixer');
-var cssnano = require('cssnano');
+var postcss = require("gulp-postcss");
+var gulp = require("gulp");
+var autoprefixer = require("autoprefixer");
+var cssnano = require("cssnano");
 
-gulp.task('default', function() {
-  var plugins = [
-    autoprefixer({ browsers: ['last 7 version', 'IOS >= 8'] }),
-    cssnano(),
-  ];
+gulp.task("default", function() {
+  var plugins = [autoprefixer(), cssnano()];
   return gulp
-    .src('./src/*.css')
+    .src("./src/*.css")
     .pipe(postcss(plugins))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest("./dist"));
 });
